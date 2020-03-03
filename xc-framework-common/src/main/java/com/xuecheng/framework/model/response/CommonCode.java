524ae471177dda87a2b1cb79249a08562d0fa6e0
ruipeng <ruiace@126.com>
2020-03-03 11:22:14 +0800

    second commit

--- xc-framework-common/src/main/java/com/xuecheng/framework/model/response/CommonCode.java
+++ xc-framework-common/src/main/java/com/xuecheng/framework/model/response/CommonCode.java
@@ -2,11 +2,9 @@ package com.xuecheng.framework.model.response;
 
 import lombok.ToString;
 
+
 /**
- * @Author: mrt.
- * @Description:
- * @Date:Created in 2018/1/24 18:33.
- * @Modified By:
+ * @author rp
  */
 
 @ToString
